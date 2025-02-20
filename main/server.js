da2edb8632bdc8924def5bf6ecadf02a701f11d5
Ítalo de Andrade <italolande@gmail.com>
2025-02-20 14:30:28 -0300

    db att
    In this update, I tried to fix some of the issues I was having with the MongoDB connection and app initialization.

--- main/server.js
+++ main/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const path = require('path');
-const db = require('./db/consql');  
-const mongo = require('./db/conmongo').MongoClient;
 const app = express();
+const path = require('path');
+const db = require('./db/consql')
+const {mongo, mongoconn} = require('./db/conmongo');
+const { DEFAULT_MIN_VERSION } = require('tls');
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.get('/' , (req,res)=>{
-  res.sendFile(path.join(__dirname, 'home' , 'main.html'))
-})
+mongoconn();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home', 'main.html')); 
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
