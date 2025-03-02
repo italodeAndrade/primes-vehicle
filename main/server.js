92ceb0c1489be8875b7739a0bce6dd893c8b06cd
Ítalo de Andrade <italolande@gmail.com>
2025-03-02 15:16:36 -0300

    cars view container

--- main/server.js
+++ main/server.js
@@ -19,6 +19,28 @@ app.get('/', (req, res) => {
     res.render("main")
 });
 
+app.get('/load_cars', (req, res) => {
+    const query = 'SELECT id, model, brand, km_driven, color, switch, nick FROM cars';
+    
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching cars:', err);
+            return res.status(500).send('Error fetching cars');
+        }
+
+        const formattedCars = results.map(car => ({
+            id: car.id,
+            model: car.model,
+            brand: car.brand,
+            km_driven: car.km_driven,
+            color: car.color,
+            switch: car.switch,
+            nick: car.nick
+        }));
+        res.json(formattedCars);
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
