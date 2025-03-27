84dbb23826642910cd861ed8e9554534ea0674ec
Ítalo de Andrade <italolande@gmail.com>
2025-03-27 15:36:03 -0300

    att

--- main/server.js
+++ main/server.js
@@ -9,29 +9,34 @@ const cloudinary = require('./db/cloud.js');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'home'));
 app.use(express.static(path.join(__dirname, 'features')));
 app.use(express.static(path.join(__dirname, 'home')));
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
     useTempFiles: true, 
     tempFileDir: '/tmp/' 
 }));
-
-
 app.use(session({
-    secret: 'meu-segredo', // Chave secreta para assinar o ID da sessão
+    secret: 'mySecretKey',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Use 'secure: true' em produção com HTTPS
-  }));
+    cookie: { secure: false }
+}));
+
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
 
 
 require('dotenv').config();
 mongoconn();
-
+            //rotas
 app.get('/login', (req,res)=> {
     res.render('user/login.ejs')
 })
@@ -43,6 +48,7 @@ app.get('/register', (req,res)=> {
 app.get('/', (req, res) => {
     res.render("main")
 });
+           //rotas
 
 app.get('/load_cars', async (req, res) => {
     const query = 'SELECT price, year, id, model, brand, km_driven, color, switch, nick FROM cars';
@@ -125,25 +131,39 @@ app.get('/load_dt/:id', (req, res) => {
     });
 });
 
-app.get('login_user', (req, res) => {
+app.post('/login_user', async (req, res) => {
     const { email, password } = req.body;
     const query = 'SELECT * FROM users WHERE email = $1 AND password = $2';
-    db.query(query, [email, password], (err, result) => {
-        if (err) {
-            console.error('Error fetching user:', err);
-            return res.status(500).send({ error: 'Error fetching user' });
-        }
-
-        if (result.rows.length === 0) {
-            return res.status(404).send({ error: 'User not found' });
-        }
-
-        req.session.user = result.rows[0];
-        res.status(200).send({ success: 'User logged in successfully' });
+    
+    db.query(query, [email, password], async (err, result) => {
+      if (err) {
+        console.error('Error fetching user:', err);
+        return res.status(500).send({ error: 'Error fetching user' });
+      }
+      if (result.rows.length === 0) {
+        return res.status(404).send({ error: 'User not found' });
+      }
+      
+      let userImage;
+      try {
+        // Supondo que você queira buscar a imagem usando o ID do usuário
+        const cloudinaryResponse = await cloudinary.api.resources_by_tag(result.rows[0].id.toString(), { max_results: 1 });
+        userImage = cloudinaryResponse.resources.map(resource => resource.secure_url);
+      } catch (error) {
+        console.error('Error fetching user images:', error);
+        return res.status(500).send({ error: 'Error fetching user images' });
+      }
+      
+      req.session.user = {
+        id: result.rows[0].id,
+        name: result.rows[0].name,
+        photo: userImage && userImage.length > 0 ? userImage[0] : null
+      };
+      
+      res.status(200).send({ success: 'User logged in successfully' });
     });
-
-});
-
+  });
+  
 app.post('/register_user', (req, res) => {
     const { name, dt_birth, cpf, address, phone, email, password } = req.body;
 
@@ -178,6 +198,32 @@ app.post('/register_user', (req, res) => {
 
         res.status(201).send({ success: 'User registered successfully' });
     });
+});
+
+app.get("/profile/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = 'SELECT * FROM users WHERE id = $1';
+    if (req.session.user) {  
+        try {
+            const result = await db.query(query, [id]);
+            if (result.rows.length === 0) {
+                return res.status(404).send({ error: 'User not found' });
+            }
+            const userFromDB = result.rows[0]; 
+            const cloudinaryResponse = await cloudinary.api.resources_by_tag(userFromDB.id.toString(), { max_results: 1 });
+            const userImages = cloudinaryResponse.resources.map(resource => resource.secure_url);
+
+            userFromDB.images = userImages; 
+            res.render('user/profile', { user: userFromDB });
+        } 
+        catch (error) {
+            console.error('Error fetching user or images:', error);
+            return res.status(500).send({ error: 'Error fetching user or images' });
+        }
+    }
+    else {
+        res.redirect('/login');  
+    }
 });
 
 app.listen(PORT, () => {
